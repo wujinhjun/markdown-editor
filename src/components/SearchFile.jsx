7bc7d22ffe48d7b1aae1e69587b3d84c50405f57
wujinhjun <wujinhjun@sjtu.edu.cn>
2022-09-03 23:12:05 +0800

    feat: the static searchFile component

---
+++ src/components/SearchFile.jsx
@@ -0,0 +1,13 @@
+import "./SearchFile.scss";
+import search from "../static/search.svg";
+
+const SearchFile = (props) => {
+  return (
+    <div id="search-file">
+      <div className="search-title">文档</div>
+      <img src={search} alt="" />
+    </div>
+  );
+};
+
+export default SearchFile;
