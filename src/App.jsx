e1424e164e23e8bcb9b88841ca798c4932dff838
wujinhjun <wujinhjun@sjtu.edu.cn>
2022-09-03 23:13:21 +0800

    feat: achieve gridded layout by flex

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,14 @@
+import SearchFile from "./components/SearchFile";
+import "./App.scss";
 function App() {
-  return <div>hello</div>;
+  return (
+    <div id="app">
+      <div className="left-panel">
+        <SearchFile></SearchFile>
+      </div>
+      <div className="right-panel"></div>
+    </div>
+  );
 }
 
 export default App;
