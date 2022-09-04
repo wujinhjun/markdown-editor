f981c00baa584ab4fc8e6f24a7871cc229d77772
wujinhjun <wujinhjun@sjtu.edu.cn>
2022-09-04 22:28:13 +0800

    feat: import fileList component

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,14 @@
+// import components
 import SearchFile from "./components/SearchFile";
+import FileList from "./components/FileList";
+
 import "./App.scss";
 function App() {
   return (
     <div id="app">
       <div className="left-panel">
         <SearchFile></SearchFile>
+        <FileList></FileList>
       </div>
       <div className="right-panel"></div>
     </div>
