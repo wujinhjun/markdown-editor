8618ec08a300c0b0b0f57a1ee11ecdf202540125
wujinhjun <wujinhjun@sjtu.edu.cn>
2022-09-05 22:46:42 +0800

    feat: simple fileTable

---
+++ src/components/FileTable.jsx
@@ -0,0 +1,11 @@
+import "./FileTable.scss";
+
+const FileTable = (props) => {
+  return (
+    <ul className="file-tables">
+      <li className="file-table">文字</li>
+    </ul>
+  );
+};
+
+export default FileTable;
