6935dc08d7735e1af3d79d29fa72fce0f0d3d87e
wujinhjun <wujinhjun@sjtu.edu.cn>
2022-09-06 17:21:33 +0800

    test: an obj to test the function

---
+++ src/utils/object.js
@@ -0,0 +1,19 @@
+export const obj = {
+    data: [
+        {
+            id: 1,
+            title: "hello-1",
+            saved: true,
+        },
+        {
+            id: 2,
+            title: "hello-2",
+            saved: true
+        },
+        {
+            id: 3,
+            title: "hello-3",
+            saved: false
+        }
+    ]
+}
