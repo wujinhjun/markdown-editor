c8951f1dc813cabdc0c37924a566d8315c9b051e
wujinhjun <wujinhjun@sjtu.edu.cn>
2022-09-08 22:16:02 +0800

    test: change the assistant file

--- src/utils/object.js
+++ src/utils/object.js
@@ -3,17 +3,14 @@ export const obj = {
         {
             id: 1,
             title: "hello-1",
-            saved: true,
         },
         {
             id: 2,
             title: "hello-2",
-            saved: true
         },
         {
             id: 3,
             title: "hello-3",
-            saved: false
         }
     ]
 }
