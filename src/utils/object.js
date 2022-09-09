8c78438f2e3565cda83509b90001c0f2d1a8b7ff
wujinhjun <wujinhjun@sjtu.edu.cn>
2022-09-09 16:29:56 +0800

    test: update the test data

--- src/utils/object.js
+++ src/utils/object.js
@@ -3,14 +3,17 @@ export const obj = {
         {
             id: 1,
             title: "hello-1",
+            body: "# hello-1"
         },
         {
             id: 2,
             title: "hello-2",
+            body: "# hello-2"
         },
         {
             id: 3,
             title: "hello-3",
+            body: "# hello-3"
         }
     ]
 }
