9633efa6ac3004fe66138520f1d8ebc0faf27320
wujinhjun <wujinhjun@sjtu.edu.cn>
2022-09-11 16:54:59 +0800

    feat: finished the CRUD for file from system

--- preload.js
+++ preload.js
@@ -1,8 +1,44 @@
-const { contextBridge, ipcRenderer, app } = require("electron");
-// const app = require("@electron/remote").app;
+// import { tranObjToArr } from "./src/utils/Helpers";
+
+const { contextBridge, ipcRenderer, dialog } = require("electron");
+const { join, dirname } = require("path");
+const fs = require("node:fs/promises");
+const Store = require("electron-store");
+
+const filesStore = new Store({ name: "FilesData" })
 const ipcTypes = require("./ipcTypes");
 
+
+
+const tranObjToArr = (objList) => {
+    // console.log(Object.keys(objList).map((item) => (objList[item])));
+    return Object.keys(objList).map((item) => (objList[item]));
+}
+
 contextBridge.exposeInMainWorld("myApp", {
+    readFile: (path) => {
+        return fs.readFile(path, { encoding: "utf-8" });
+    },
+
+    writeFile: (path, content) => {
+        return fs.writeFile(path, content, { encoding: "utf-8" })
+    },
+
+    renameFile: (path, newPath) => {
+        return fs.rename(path, newPath);
+    },
+
+    deleteFile: (path) => {
+        return fs.unlink(path);
+    },
+
+    joinPath: (path, title) => {
+        return join(path, `${title}.md`)
+    },
+    dirPath: (path) => {
+        return dirname(path);
+    },
+
     getPath: (title) => (
         ipcRenderer.invoke(ipcTypes.GET_PATH_NAME, title)
             .then((res) => {
@@ -13,5 +49,38 @@ contextBridge.exposeInMainWorld("myApp", {
                 // console.log(err);
                 return err;
             })
-    )
+    ),
+
+    getFilesData: () => (
+        filesStore.get("files")
+    ),
+
+    saveFilesData: (files) => {
+        const filesStoreObj = tranObjToArr(files).reduce((result, file) => {
+            const { id, path, title } = file;
+            result[id] = {
+                id,
+                path,
+                title
+            };
+            return result;
+        }, {});
+        filesStore.set("files", filesStoreObj);
+        return;
+    },
+
+    showDialog: () => {
+        dialog.showOpenDialog({
+            title: "选择要导入的文件",
+            properties: ["openFile", "multiSelections"],
+            filters: [
+                {
+                    name: "markdown files",
+                    extensions: ["md"]
+                }
+            ],
+        }).then((paths) => {
+            console.log(paths);
+        })
+    }
 })
