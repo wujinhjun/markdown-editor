1daf00e49b8d2d49869e7b1fad26ff9bd1d3579a
wujinhjun <wujinhjun@sjtu.edu.cn>
2022-09-11 16:55:52 +0800

    refactor: change the implementation of io-api

--- src/utils/fileDealer.js
+++ src/utils/fileDealer.js
@@ -1,23 +1,27 @@
-const fs = require('node:fs/promises');
-const path = require("path");
+// const fs = window.require('node:fs/promises');
+// const path = window.require("path");
 const fileDealer = {
     readFile: (path) => {
-        console.log(path);
-        return fs.readFile(path, { encoding: 'utf-8' });
+        // console.log(path);
+        // return fs.readFile(path, { encoding: 'utf-8' });
+        return window.myApp.readFile(path);
     },
 
     writeFile: (path, content) => {
-        console.log(path);
-        return fs.writeFile(path, content, { encoding: 'utf-8' });
+        // console.log(path);
+        // return fs.writeFile(path, content, { encoding: 'utf-8' });
+        return window.myApp.writeFile(path, content);
     },
 
     renameFile: (path, newPath) => {
-        console.log(newPath);
-        return fs.rename(path, newPath);
+        // console.log(newPath);
+        // return fs.rename(path, newPath);
+        return window.myApp.renameFile(path, newPath);
     },
 
     deleteFile: (path) => {
-        return fs.unlink(path);
+        // return fs.unlink(path);
+        return window.myApp.deleteFile(path);
     }
 }
 
