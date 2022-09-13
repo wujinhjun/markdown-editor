433611ebff84177569008fde2ec21c106bd74821
wujinhjun <wujinhjun@sjtu.edu.cn>
2022-09-13 15:15:03 +0800

    feat: add new ipc action

--- ipcTypes.js
+++ ipcTypes.js
@@ -2,3 +2,4 @@ exports.GET_PATH_NAME = "get-path";
 exports.OPEN_DIALOG = "open-dialog";
 exports.IMPORT_MESSAGE = "import-message";
 exports.IMPORT_ERROR = "import-error";
+exports.SAVE_EDIT_FILE = "save_edit_file";
