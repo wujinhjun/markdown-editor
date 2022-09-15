95bdade7cf121f67eb1e022d84f2864e73a3b1fa
wujinhjun <wujinhjun@sjtu.edu.cn>
2022-09-15 16:52:21 +0800

    feat: packaging myself hooks

---
+++ src/hooks/useContextmenu.js
@@ -0,0 +1,29 @@
+import { useRef } from "react"
+import { useEffect } from "react"
+
+const useContextmenu = (targetElement, devList) => {
+    const clickElement = useRef(null);
+
+    useEffect(() => {
+        const handleElement = (e) => {
+            console.log(targetElement);
+            if (document.querySelector(targetElement).contains(e.target)) {
+                clickElement.current = e.target;
+                e.preventDefault();
+                window.myApp.openContextDialog();
+            }
+
+        };
+
+        window.addEventListener("contextmenu", handleElement);
+
+        return () => {
+            window.removeEventListener("contextmenu", handleElement);
+        }
+
+    }, devList);
+
+    return clickElement;
+}
+
+export default useContextmenu;
