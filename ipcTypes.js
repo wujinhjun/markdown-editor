05c208dd736c63869d798efd139708a6822cb24e
wujinhjun <wujinhjun@sjtu.edu.cn>
2022-09-16 12:36:48 +0800

    feat: add some new action

--- ipcTypes.js
+++ ipcTypes.js
@@ -1,17 +1,25 @@
+// main window config
 exports.GET_PATH_NAME = "get-path";
-exports.OPEN_DIALOG = "open-dialog";
 
+// about import
+exports.OPEN_DIALOG = "open-dialog";
 exports.IMPORT_MESSAGE = "import-message";
 exports.IMPORT_ERROR = "import-error";
 
+// about menu ang shortcut
 exports.CREATE_NEW_FILE = "create-new-file";
 exports.SAVE_EDIT_FILE = "save-edit-file";
 exports.SEARCH_FILE = "search-file";
 exports.IMPORT_FILE = "import-file";
 exports.CLOSE_FILE = "close-file";
 
+// about context menu
 exports.OPEN_CONTEXT_MENU = "open-context-menu";
-
 exports.RENAME_FILE = "rename-file";
 exports.DELETE_FILE = "delete-file";
 exports.OPEN_FILE = "open-file";
+
+// setting window config
+exports.OPEN_SETTING_WINDOW = "open-setting-window";
+exports.OPEN_LOCATION_DIALOG = "open-location-dialog";
+exports.SAVE_LOCATION_PATH = "save-location-path";
