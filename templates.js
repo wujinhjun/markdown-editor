240cd894208a8cc3680df16a0508209e6b2d0b49
wujinhjun <wujinhjun@sjtu.edu.cn>
2022-09-17 13:02:26 +0800

    feat: more function menu

--- templates.js
+++ templates.js
@@ -42,6 +42,44 @@ let template = [
         ]
     },
     {
+        label: "编辑",
+        submenu: [
+            {
+                label: '撤销',
+                accelerator: 'CmdOrCtrl+Z',
+                role: 'undo'
+            },
+            {
+                label: '重做',
+                accelerator: 'Shift+CmdOrCtrl+Z',
+                role: 'redo'
+            },
+            {
+                type: 'separator'
+            },
+            {
+                label: '剪切',
+                accelerator: 'CmdOrCtrl+X',
+                role: 'cut'
+            },
+            {
+                label: '复制',
+                accelerator: 'CmdOrCtrl+C',
+                role: 'copy'
+            },
+            {
+                label: '粘贴',
+                accelerator: 'CmdOrCtrl+V',
+                role: 'paste'
+            },
+            {
+                label: '全选',
+                accelerator: 'CmdOrCtrl+A',
+                role: 'selectall'
+            }
+        ]
+    },
+    {
         label: "软件设置",
         submenu: [
             {
@@ -49,21 +87,6 @@ let template = [
                 accelerator: "CmdOrCtrl+,",
                 click: (menuItem, browserWindow, event) => {
                     browserWindow.send(ipcTypes.OPEN_SETTING_WINDOW);
-                }
-            },
-
-        ]
-    },
-    {
-        label: "视图",
-        submenu: [
-            {
-                label: "刷新",
-                accelerator: "CmdOrCtrl+R",
-                click: (item, focusedWindow) => {
-                    if (focusedWindow) {
-                        focusedWindow.reload();
-                    }
                 }
             },
 
